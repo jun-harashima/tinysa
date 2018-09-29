381a876f4724800c70db57120c4c0257154d6717
Jun Harashima <jun-harashima@cookpad.com>
2018-09-29 16:51:28 +0900

    add _sort_suffixes_between

--- suffix_array/suffix_array.py
+++ suffix_array/suffix_array.py
@@ -1,13 +1,36 @@
+import os
+
 class SuffixArray():
 
     def __init__(self):
         self.suffixes = []
-        self.starting_positions = []
+        self.positions = []
 
     def index(self, text):
-        self.starting_positions = [5, 3, 1, 0, 4, 2]
+        self.positions = [5, 3, 1, 0, 4, 2]
 
     def _get_suffxies_of(self, text):
         for i in range(len(text)):
             self.suffixes.append(text[i:])
+            self.positions.append(i)
 
+    def _sort_suffixes_between(self, i, j):
+        p_suffix = self.suffixes[i] # pivot suffix
+        p_position = self.positions[i] # pivot position
+        l_suffixes = [] # left suffixes
+        r_suffixes = [] # right suffixes
+        l_positions = [] # left positions
+        r_positions = [] # right positions
+        for x in range(i + 1, j + 1):
+            if self.suffixes[x] <= p_suffix:
+                l_suffixes.append(self.suffixes[x])
+                l_positions.append(self.positions[x])
+            else:
+                r_suffixes.append(self.suffixes[x])
+                r_positions.append(self.positions[x])
+        self.suffixes[i:j+1] = l_suffixes + [p_suffix] + r_suffixes
+        self.positions[i:j+1] = l_positions + [p_position] + r_positions
+        if len(l_suffixes) > 1:
+            self._sort_suffixes_between(i, len(l_suffixes) - 1)
+        if len(r_suffixes) > 1:
+            self._sort_suffixes_between(len(l_suffixes) + 1, j)
--- tests/test_suffix_array.py
+++ tests/test_suffix_array.py
@@ -7,13 +7,23 @@ class TestSuffix_array(unittest.TestCase):
     def test_index(self):
         suffix_array = SuffixArray()
         suffix_array.index('banana')
-        self.assertEqual(suffix_array.starting_positions, [5, 3, 1, 0, 4, 2])
+        self.assertEqual(suffix_array.positions, [5, 3, 1, 0, 4, 2])
 
     def test__get_suffixes(self):
         suffix_array = SuffixArray()
         suffix_array._get_suffxies_of('banana')
         suffixes = ['banana', 'anana', 'nana', 'ana', 'na', 'a']
         self.assertEqual(suffix_array.suffixes, suffixes)
+
+
+    def test__sort_suffixes_between(self):
+        suffix_array = SuffixArray()
+        suffix_array._get_suffxies_of('banana')
+        suffix_array._sort_suffixes_between(0, len(suffix_array.suffixes) - 1)
+        sorted_suffixes = ['a', 'ana', 'anana', 'banana', 'na', 'nana']
+        sorted_positions = [5, 3, 1, 0, 4, 2]
+        self.assertEqual(suffix_array.suffixes, sorted_suffixes)
+        self.assertEqual(suffix_array.positions, sorted_positions)
 
 
 if __name__ == "__main__":
