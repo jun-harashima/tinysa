8ad69e8acd7b7bbfe8c7b38bfe1318081e22cfaf
Jun Harashima <jun-harashima@cookpad.com>
2018-09-29 13:43:35 +0900

    add _get_suffxies_of

--- suffix_array/suffix_array.py
+++ suffix_array/suffix_array.py
@@ -1,7 +1,13 @@
 class SuffixArray():
 
     def __init__(self):
+        self.suffixes = []
         self.starting_positions = []
 
     def index(self, text):
         self.starting_positions = [5, 3, 1, 0, 4, 2]
+
+    def _get_suffxies_of(self, text):
+        for i in range(len(text)):
+            self.suffixes.append(text[i:])
+
